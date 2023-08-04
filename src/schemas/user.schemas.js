02114ab49334b8211854faeebfac064af1667bbe
andressafirmino <andressafirmino78@gmail.com>
2023-08-04 16:07:22 -0300

    fix: signUpSchema

--- src/schemas/user.schemas.js
+++ src/schemas/user.schemas.js
@@ -3,8 +3,8 @@ import joi from "joi";
 export const signUpSchema = joi.object({
     name: joi.string().min(1).required(),
     email: joi.string().email().required(),
-    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    confirmPassword: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+    password: joi.string().pattern(new RegExp('^[\\w!@#$%^&*()\\-+=<>?/\\\\[\\]{}|;:\'",.]{3,}$')).required(),
+    confirmPassword: joi.string().pattern(new RegExp('^[\\w!@#$%^&*()\\-+=<>?/\\\\[\\]{}|;:\'",.]{3,}$')).required()
 })
 
 export const signInSchema = joi.object({
