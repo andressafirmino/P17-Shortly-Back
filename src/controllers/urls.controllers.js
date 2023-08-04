289ea4727b6559ebcbe3ce2a1cfc34cddff59a0c
andressafirmino <andressafirmino78@gmail.com>
2023-08-04 16:28:35 -0300

    feat: POST/urls/shorten res.send configurado

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -10,8 +10,12 @@ export async function postUrls(req, res) {
         if (logged.rows.length === 0) {
             return res.status(401).send({ message: "Usuário não autorizado!" });
         }
+        const user = await db.query(`SELECT * FROM users WHERE email = $1;`, [logged.rows[0].email]);
         const shortUrl = nanoid(8);
-        res.status(201).send({shortUrl});
+        await db.query(`INSERT INTO urls ("shortUrl", url) VALUES ($1, $2);`, [shortUrl, url]);
+        const short = await db.query(`SELECT * FROM urls WHERE "shortUrl" = $1;`, [shortUrl]);
+        await db.query(`INSERT INTO shorts ("userId", "shortId") VALUES ($1, $2);`, [user.rows[0].id, short.rows[0].id]);
+        res.status(201).send({id: short.rows[0].id, shortUrl});
     } catch (e) {
         res.status(500).send(e.message);
     }
