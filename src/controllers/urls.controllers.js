91ab2f75246cd93dbf8528ebf3e0e68123ef466a
andressafirmino <andressafirmino78@gmail.com>
2023-08-04 15:32:15 -0300

    feat: short url

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,15 +1,17 @@
+import { nanoid } from "nanoid";
 import { db } from "../database/database.js";
 
-export async function postUrls (req, res) {
+export async function postUrls(req, res) {
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer ", "");
-    console.log(token);
+    const { url } = req.body;
     try {
         const logged = await db.query(`SELECT * FROM logged WHERE token = $1;`, [token]);
-        if(logged.rows.length === 0) {
-            return res.status(401).send({message: "Usuário não autorizado!"});
+        if (logged.rows.length === 0) {
+            return res.status(401).send({ message: "Usuário não autorizado!" });
         }
-        res.sendStatus(201);
+        const shortUrl = nanoid(8);
+        res.status(201).send({shortUrl});
     } catch (e) {
         res.status(500).send(e.message);
     }
