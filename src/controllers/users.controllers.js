d56bfeb24de1657399346572973a77d5ba9ca67a
andressafirmino <andressafirmino78@gmail.com>
2023-08-05 22:24:04 -0300

    refactor: repository users

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,7 +1,6 @@
-import { db } from "../database/database.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-import { createUser } from "../repository/users.repository.js";
+import { allUser, checkEmail, checkToken, createUser, login } from "../repository/users.repository.js";
 
 export async function signUp(req, res) {
     const { name, email, password, confirmPassword } = req.body;
@@ -10,12 +9,12 @@ export async function signUp(req, res) {
         if (password !== confirmPassword) {
             return res.status(422).send({ message: "As senhas são diferentes!" });
         }
-        const user = await createUser(name);
+        const user = await checkEmail(email);
         if (user.rows.length !== 0) {
             return res.status(409).send({ message: "Usuário já cadastrado!" });
         }
         const hash = bcrypt.hashSync(password, 10);
-        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, hash]);
+        await createUser(name, email, hash);
         res.sendStatus(201);
     } catch (e) {
         res.status(500).send(e.message);
@@ -26,7 +25,7 @@ export async function signIn(req, res) {
     const { email, password } = req.body;
 
     try {
-        const user = await db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+        const user = await createUser(email);
         if (user.rows.length === 0) {
             return res.status(401).send({ message: "Usuário e/ou senha inválido!" });
         }
@@ -35,7 +34,7 @@ export async function signIn(req, res) {
             return res.status(401).send({ message: "Usuário e/ou senha inválido!" });
         }
         const token = uuid();
-        await db.query(`INSERT INTO logged (name, email, token) VALUES ($1, $2, $3);`, [user.rows[0].name, email, token]);
+        await login(user.rows[0].name, email, token);
         res.status(200).send({ token });
     } catch (e) {
         res.status(500).send(e.message);
@@ -47,22 +46,11 @@ export async function getUser(req, res) {
     const token = authorization?.replace("Bearer ", "");
 
     try {
-        const logged = await db.query(`SELECT * FROM logged WHERE token = $1;`, [token]);
+        const logged = await checkToken(token);
         if (logged.rows.length === 0) {
             return res.status(401).send({ message: "Usuário não autorizado!" });
         }
-
-        const userMe = await db.query(`SELECT users.id, users.name,
-        CAST(SUM(urls."visitCount")AS INTEGER) AS "visitCount",
-        json_agg(
-            json_build_object('id', urls.id, 'shortUrl', urls."shortUrl", 'url', urls.url, 'visitCount', urls."visitCount")
-        ) AS "shortenedUrls"
-        FROM users
-        JOIN shorts ON shorts."userId" = users.id
-        JOIN urls ON urls.id = shorts."shortId"
-        WHERE users.email = $1
-        GROUP BY users.id, users.name
-    ;`, [logged.rows[0].email]);
+        const userMe = await allUser(logged.rows[0].email);
         res.status(200).send(userMe.rows[0]);
     } catch (e) {
         res.status(500).send(e.message);
--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -1,6 +1,36 @@
 import { db } from "../database/database.js";
 
-export async function createUser(email) {
-    const user = await db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
-    return user;
+export function checkEmail(email) {
+    const res = db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+    return res;
+}
+
+export function createUser(name, email, hash) {
+    const res = db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, hash]);
+    return res;
+}
+
+export function login(name, email, token) {
+    const res = db.query(`INSERT INTO logged (name, email, token) VALUES ($1, $2, $3);`, [name, email, token])
+    return res;
+}
+
+export function checkToken(token) {
+    const res = db.query(`SELECT * FROM logged WHERE token = $1;`, [token]);
+    return res;
+}
+
+export function allUser(email) {
+    const res = db.query(`SELECT users.id, users.name,
+    CAST(SUM(urls."visitCount")AS INTEGER) AS "visitCount",
+    json_agg(
+        json_build_object('id', urls.id, 'shortUrl', urls."shortUrl", 'url', urls.url, 'visitCount', urls."visitCount")
+    ) AS "shortenedUrls"
+    FROM users
+    JOIN shorts ON shorts."userId" = users.id
+    JOIN urls ON urls.id = shorts."shortId"
+    WHERE users.email = $1
+    GROUP BY users.id, users.name
+    ;`, [email]);
+    return res;
 }
